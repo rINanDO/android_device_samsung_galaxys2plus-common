c8146012a668695916e96fd24f2c58f16226603d
ghsr <ghsr92@yandex.ru>
2015-10-20 12:12:17 +0600

    galaxys2plus: Add media_profiles.xml

--- common.mk
+++ common.mk
@@ -59,7 +59,8 @@ USE_CUSTOM_AUDIO_POLICY := 1
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
