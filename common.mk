4353b69842f3150d8f48299addc2b46f54475592
ghsr <ghsr92@yandex.ru>
2015-10-28 23:40:31 +0600

    galaxys2plus: Remove com.android.future.usb.accessory
     It already exists in core_minimal.mk

--- common.mk
+++ common.mk
@@ -72,8 +72,7 @@ PRODUCT_COPY_FILES += \
 # Packages
 PRODUCT_PACKAGES += \
     SamsungServiceMode \
-    charger_res_images \
-    com.android.future.usb.accessory
+    charger_res_images
 
 # Permissions
 PRODUCT_COPY_FILES += \
