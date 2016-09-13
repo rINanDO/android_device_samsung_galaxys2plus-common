ee8f36b7aca1d779facc68bb500aebe0453078b0
Dominggoes Isakh <drjisakh@gmail.com>
2016-09-13 20:42:22 +0100

    galaxys2plus-common: Add TWRP support
    Change-Id: Iaf6b7fa179928479be3e4f9465aeb57e9c823e51

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,3 +105,14 @@ MALLOC_IMPL := dlmalloc
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/galaxys2plus-common/sepolicy
+
+# TWRP
+TW_THEME := portrait_mdpi
+TWRP_NEW_THEME := true
+HAVE_SELINUX := true
+TW_MAX_BRIGHTNESS := 255
+TW_INCLUDE_CRYPTO := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/platform/s5p-tmu/temperature"
